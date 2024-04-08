5d9619ecb953abd4c88662d285b60ea0ded0028d
Michael Mensah <michaelmensa@live.com>
2024-04-08 15:29:20 +0000

    added task_4

---
+++ react/react-intro/task_4/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label>Email: 
+          <input for="email" type="email"></input>
+        </label>
+        <label>Password: 
+          <input for="password" type="password"></input>
+        </label>
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
